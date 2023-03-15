435a1f9651e64a522540bda0ec436a041c618615
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-03-15 15:14:59 +0000

    Add aggressive validation

--- script.js
+++ script.js
@@ -3,14 +3,29 @@ const passwordInput = document.querySelector('#password')
 const confirmInput = document.querySelector('#password-confirm')
 const confirmMsg = document.querySelector('.confirm-msg')
 const passwordMsg = document.querySelector('.password-msg')
+const emailMsg = document.querySelector('.email-msg')
+const emailInput = document.querySelector('#email')
+const submitBtn = document.querySelector('.create-account-btn')
+const nameMsg = document.querySelectorAll('.name-msg')
+const firstNameInput = document.querySelector('#first-name')
+const lastNameInput = document.querySelector('#last-name')
+const allInputs = document.querySelectorAll('input')
 
-passwordInput.addEventListener('input', validationMsg)
+
+let feedback = false 
+
+passwordInput.addEventListener('blur', validationMsg)
+passwordInput.addEventListener('input', aggressiveFeedback)
 confirmInput.addEventListener('input', matchPasswords)
+emailInput.addEventListener('blur', validateEmail)
+emailInput.addEventListener('input', aggressiveFeedback)
+submitBtn.addEventListener('click', validateForm)
+
 
 //CONFIRM IF PASSWORDS MATCH
 
-function matchPasswords(e) {
-    if(e.target.value === passwordInput.value) {
+function matchPasswords() {
+    if(confirmInput.value === passwordInput.value && passwordInput.value !== '') {
         confirmMsg.textContent = "Passwords match ✓"
         confirmMsg.classList.add("valid");
         confirmMsg.classList.remove("invalid")
@@ -30,17 +45,62 @@ function validatePassword(password) {
 
 
 function validationMsg() {
+    feedback = true
     let input = passwordInput.value
     if(validatePassword(input)) {
         passwordMsg.textContent = "✓"
         passwordMsg.classList.add("valid")
+        passwordMsg.classList.remove("invalid")
     }
     else {
         passwordMsg.textContent = 
         "Password must be atleast 8 characters long, contain 1 uppercase letter, 1 number and 1 special character"
         passwordMsg.classList.add("invalid")
+        passwordMsg.classList.remove("valid")
     }
 }
+
+// UPDATE MSG ONLY WHEN USER CLICKS AWAY BUT WHEN USER CLICKS BACK INTO FIELD, UPDATE IN REAL TIME
+
+function aggressiveFeedback() {
+    if(feedback && passwordInput.value !== '') {
+        validationMsg()
+     } else if (feedback && emailInput.value !== '') {
+        validateEmail()
+     }
+}
+
+// EMAIL FEEDBACK
+
+function validateEmail() {
+    feedback = true
+    let validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if(emailInput.value.match(validEmail)) {
+        emailMsg.textContent = "✓"
+        emailMsg.classList.add("valid")
+        emailMsg.classList.remove("invalid")
+    } else {
+        emailMsg.textContent = "Please enter a valid email address"
+        emailMsg.classList.add("invalid")
+        emailMsg.classList.remove("valid")
+    }
+}
+
+// DISPLAY REQUIRED MESSAGE BELOW EMPTY FIELDS WHEN BUTTON IS CLICKED
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
